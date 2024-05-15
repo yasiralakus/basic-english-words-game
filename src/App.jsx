72eb48a6219ae446089637e0af5f676459bc3a00
Yasir Alakus <yasiralakus11@gmail.com>
2024-05-15 17:28:04 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,15 @@ export default function App() {
     const [choise2, setChoise2] = useState(null);
     const [choise3, setChoise3] = useState(null);
     const [choise4, setChoise4] = useState(null);
-    const [isTrue, setIsTrue] = useState(null);
-    const [isClick, setIsClick] = useState(false);
+    const [clicked1, setClicked1] = useState(null);
+    const [clicked2, setClicked2] = useState(null);
+    const [clicked3, setClicked3] = useState(null);
+    const [clicked4, setClicked4] = useState(null);
+    const [darkMode, setDarkMode] = useState(false);
 
-    function randomNumber() {
-        return Math.floor(Math.random() * 1000)
+
+    function randomNumber(){
+        return Math.floor(Math.random() * 998)
     }
 
     function shuffleArray(array) {
@@ -26,68 +30,87 @@ export default function App() {
         return array;
       }
 
-    function newQuestion(e) {
-        const sayi1 = randomNumber();
-        const sayi2 = randomNumber();
-        const sayi3 = randomNumber();
-        const sayi4 = randomNumber();
+    function newQuestion() {
+        setClicked1(null);
+        setClicked2(null);
+        setClicked3(null);
+        setClicked4(null);
+        const randomNumber1 = randomNumber();
+        const randomNumber2 = randomNumber();
+        const randomNumber3 = randomNumber();
+        const randomNumber4 = randomNumber();
 
-        const random = Math.floor(Math.random() * 4) + 1
+        const choices = [words[randomNumber1].eng, words[randomNumber2].eng, words[randomNumber3].eng, words[randomNumber4].eng];
+        var shuffledChoices = shuffleArray(choices);
 
-        setQuestion(words[sayi1].eng)
-        setAnswer(words[sayi1].tr)
-        const choises = [words[sayi1].tr,words[sayi2].tr,words[sayi3].tr,words[sayi4].tr,]
-        const shuffledChoices = shuffleArray(choises);
+        setQuestion(words[randomNumber1].tr)
+        setAnswer(words[randomNumber1].eng)
         setChoise1(shuffledChoices[0]);
         setChoise2(shuffledChoices[1]);
         setChoise3(shuffledChoices[2]);
         setChoise4(shuffledChoices[3]);
     }
 
-    function control(e) {
-        setIsClick(true);
+    function clickButton1(e) {
+        e.preventDefault();
         if(e.target.value === answer) {
-            alert('doğru')
-            newQuestion();
+            setClicked1(true)
         } else {
-            alert('yanlış')
+            setClicked1(false)
         }
     }
 
-    function showAnswer(e) {
+    function clickButton2(e) {
         e.preventDefault();
-        alert(answer)
+        if(e.target.value === answer) {
+            setClicked2(true)
+        } else {
+            setClicked2(false)
+        }
+    }
+
+    function clickButton3(e) {
+        e.preventDefault();
+        if(e.target.value === answer) {
+            setClicked3(true)
+        } else {
+            setClicked3(false)
+        }
+    }
+
+    function clickButton4(e) {
+        e.preventDefault();
+        if(e.target.value === answer) {
+            setClicked4(true)
+        } else {
+            setClicked4(false)
+        }
     }
 
     return (
 
-        <div className="full-page">
+        <div className="full-page" style={darkMode === true ? {backgroundColor: '#000'} : {}}>
 
-            <div className="choises-other">
-                <button onClick={newQuestion}>Yeni soru</button>
-                <button onClick={showAnswer}>Cevabı Öğren</button>
-            </div>
+                <button style={darkMode === true ? {backgroundColor: '#f5f5f5', color: '#000'} : {}} id="new-question" onClick={() => (darkMode === true ? setDarkMode(false) : setDarkMode(true))}>Karanlık Mod</button>
+                <button style={darkMode === true ? {backgroundColor: '#f5f5f5', color: '#000'} : {}} id="new-question" onClick={newQuestion}>Yeni Soru</button>
 
-            <div className="container">
+            <div className="container" style={darkMode === true ? {backgroundColor: '#f5f5f5', color: '#000'} : {}}>
 
                 <div className="question">
-
-                    <h1>{question === null ? 'Soru' : question}</h1>
-
+                    <h1>{question === null ? 'Yeni soru iste' : question}</h1>
                 </div>
 
-                <div className="choises">
-
-                    <button value={choise1} disabled={choise1 === null} onClick={control}>{choise1 === null ? 'A' : choise1}</button>
-                    <button value={choise2} disabled={choise2 === null} onClick={control}>{choise2 === null ? 'B' : choise2}</button>
-                    <button value={choise3} disabled={choise3 === null} onClick={control}>{choise3 === null ? 'C' : choise3}</button>
-                    <button value={choise4} disabled={choise4 === null} onClick={control}>{choise4 === null ? 'D' : choise4}</button>
-
+                <div className="choices">
+                    <button onClick={clickButton1} disabled={choise1 === null} style={{ backgroundColor: clicked1 ? '#008000' : clicked1 === false ? '#b22222' : ''}} value={choise1}>{choise1 === null ? 'A' : choise1}</button>
+                    <button onClick={clickButton2} disabled={choise2 === null} style={{ backgroundColor: clicked2 ? '#008000' : clicked2 === false ? '#b22222' : ''}} value={choise2}>{choise2 === null ? 'B' : choise2}</button>
+                    <button onClick={clickButton3} disabled={choise3 === null} style={{ backgroundColor: clicked3 ? '#008000' : clicked3 === false ? '#b22222' : ''}} value={choise3}>{choise3 === null ? 'C' : choise3}</button>
+                    <button onClick={clickButton4} disabled={choise4 === null} style={{ backgroundColor: clicked4 ? '#008000' : clicked4 === false ? '#b22222' : ''}} value={choise4}>{choise4 === null ? 'D' : choise4}</button>
                 </div>
 
             </div>
 
-        </div>
 
+        </div>
+       
     )
 }
